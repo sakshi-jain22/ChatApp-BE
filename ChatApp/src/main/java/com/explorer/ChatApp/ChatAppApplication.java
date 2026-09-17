2a745dcb4bc1e0f3d4a7dc084b316ebb02f07410
sakshi-jain22 <jain.sakshi.2204@gmail.com>
2026-09-17 17:40:24 +0530

    First Commit

---
+++ ChatApp/src/main/java/com/explorer/ChatApp/ChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.explorer.ChatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppApplication.class, args);
+	}
+
+}
